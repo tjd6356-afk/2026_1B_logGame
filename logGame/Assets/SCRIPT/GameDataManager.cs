bcb6fc99df427850820fbefce48afe9f717cff6b
tjd6356-afk <tjd6356@gmail.com>
2026-05-21 15:40:09 +0900

    ㅁㅇㅁㅇ

---
+++ logGame/Assets/SCRIPT/GameDataManager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameDataManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
