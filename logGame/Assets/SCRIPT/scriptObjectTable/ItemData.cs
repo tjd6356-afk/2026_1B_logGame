8fafb48d47bd5d5b641946fdc6943eb8a7a548bc
tjd6356_afk <tjd6356@gmail.com>
2026-06-04 15:43:31 +0900

    아이템 스탯

--- logGame/Assets/SCRIPT/scriptObjectTable/ItemData.cs
+++ logGame/Assets/SCRIPT/scriptObjectTable/ItemData.cs
@@ -8,4 +8,10 @@ public class ItemData : ScriptableObject
     [TextArea]
     public string description;   // 아이템 상세 설명
     public Sprite icon;          // 인벤토리 슬롯에 표시될 이미지
+
+    [Header("📊 아이템 장착 스탯 보너스")]
+    public int attackBonus;      // 증가할 공격력
+    public int healthBonus;      // 증가할 체력
+    public int defenseBonus;     // 증가할 방어력
+
 }
